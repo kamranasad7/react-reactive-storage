21ba66b78e4a04ff4aa71aabbcf48ded5290bbbd
Asad Kamran <kamranasad7@gmail.com>
2023-11-14 10:54:30 +0500

    package json update and files cleaned

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 // vite.config.js
 import { resolve } from 'path'
-import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts({ include: "lib" }), react()],
+  plugins: [dts({ include: "lib", insertTypesEntry: true, })],
   build: {
     copyPublicDir: false,
     lib: {
